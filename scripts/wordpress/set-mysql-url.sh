2a58c5426f4e5903b2604bf64102c497f88cbaec
tiagorol <tiago.rol@gmail.com>
2015-11-07 17:20:27 -0300

    Primeiro commit

--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -5,7 +5,9 @@ set -e
 ctx logger info $(ctx target instance host_ip)
 ctx logger info $(ctx target node properties port)
 
-echo $(ctx target instance host_ip) > /home/ubuntu/log1.log
-echo $(ctx target node properties port) > /home/ubuntu/log2.log
+sed -i "s/localhost/$(ctx target instance host_ip)/" /var/www/html/wp-config.php
+sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
+sed -i "s/username_here/wordpressuser/" /var/www/html/wp-config.php
+sed -i "s/database_name_here/wordpress/" /var/www/html/wp-config.php
 
 ctx logger info "Sucessfully IP set"
