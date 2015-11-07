b7b363244cc13c4200cb28786c790cb5b89954ad
tiagorol <tiago.rol@gmail.com>
2015-11-07 16:19:43 -0300

    Primeiro commit

--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+ctx logger info $(ctx target instance host_ip)
+ctx logger info $(ctx target node properties port)
 
-
-#ctx source instance runtime_properties mongo_ip_address $(ctx target instance host_ip)
-#ctx source instance runtime_properties mongo_port $(ctx target node properties port)
+echo $(ctx target instance host_ip) > /home/ubuntu/log1.log
+echo $(ctx target node properties port) > /home/ubuntu/log2.log
 
 ctx logger info "Sucessfully IP set"
