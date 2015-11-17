4c98f1cc134fddf1fe1be991d97aea5f6c5e4706
tiagorol <tiago.rol@gmail.com>
2015-11-17 20:04:40 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -3,10 +3,9 @@
 set -e
 
 ctx logger info $(ctx target instance host_ip)
-ctx logger info $(ctx target node properties port)
-ctx logger info $(ctx target instance host_ip)
-ctx logger info $(ctx target node properties port)
 
-sudo sed -i "s/localhost/$(ctx target instance host_ip)/" /var/www/html/wp-config.php
+cd /home/ubuntu/
+wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/scripts/crawler/wordpress_integrado.yml
+sudo sed -i "s/#WP_HOST/$(ctx target instance host_ip)/" /home/ubuntu/wordpress_integrado.yml
 
 ctx logger info "Sucessfully start monitoring"
