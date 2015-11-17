423a2fffb509f4cd16e965ae27dd84e60c9ff6b2
tiagorol <tiago.rol@gmail.com>
2015-11-17 19:59:02 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -7,4 +7,6 @@ ctx logger info $(ctx target node properties port)
 ctx logger info $(ctx target instance host_ip)
 ctx logger info $(ctx target node properties port)
 
+sudo sed -i "s/localhost/$(ctx target instance host_ip)/" /var/www/html/wp-config.php
+
 ctx logger info "Sucessfully start monitoring"
--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -3,11 +3,9 @@
 set -e
 
 ctx logger info $(ctx target instance host_ip)
-ctx logger info $(ctx target node properties port)
 
-sudo sed -i "s/localhost/$(ctx target instance host_ip)/" /var/www/html/wp-config.php
-sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
-sudo sed -i "s/username_here/wordpressuser/" /var/www/html/wp-config.php
-sudo sed -i "s/database_name_here/wordpress/" /var/www/html/wp-config.php
+cd /home/ubuntu/
+wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/scripts/crawler/wordpress_integrado.yml
+sudo sed -i "s/#WP_HOST/$(ctx target instance host_ip)/" /home/ubuntu/wordpress_integrado.yml
 
-ctx logger info "Sucessfully IP set"
+ctx logger info "Sucessfully IP set Crawler"
