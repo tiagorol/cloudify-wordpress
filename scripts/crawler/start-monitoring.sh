7a0cb0fb577a409bc7a9983acb3d55a227c18095
tiagorol <tiago.rol@gmail.com>
2015-11-21 14:37:25 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-ctx logger info $(ctx instance host_ip)
-ctx logger info $(ctx source instance runtime_properties public_ip_address)
+#ctx logger info $(ctx instance host_ip)
+#ctx logger info $(ctx source instance runtime_properties public_ip_address)
 
 #cd /home/ubuntu/
 #wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/scripts/crawler/wordpress_integrado.yml
--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-ctx logger info $(ctx target instance host_ip)
-ctx logger info $(ctx target node properties port)
+ctx logger info $(ctx instance host_ip)
+ctx logger info $(ctx source instance runtime_properties public_ip_address)
 
 sudo sed -i "s/localhost/$(ctx target instance host_ip)/" /var/www/html/wp-config.php
 sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
