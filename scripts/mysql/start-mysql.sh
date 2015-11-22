aa1e7dd73bc83e1e8aba74a155cb2c461e0b4bcf
tiagorol <tiago.rol@gmail.com>
2015-11-22 17:42:43 -0300

    .

--- scripts/mysql/start-mysql.sh
+++ scripts/mysql/start-mysql.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+ctx logger info "Pegando IP HOST..."
+
+ctx logger info $(ctx target instance host_ip)
+ctx logger info $(ctx target instance public_ip_address)
+
 ctx logger info "Sucessfully start MySQL"
