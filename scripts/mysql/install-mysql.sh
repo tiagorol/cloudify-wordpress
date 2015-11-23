8967551a63e23196dc1888e3f0de02071d5874e5
tiagorol <tiago.rol@gmail.com>
2015-11-23 20:07:24 -0300

    .

--- scripts/mysql/install-mysql.sh
+++ scripts/mysql/install-mysql.sh
@@ -5,30 +5,30 @@ set -e
 #Install MYSQL Server
 
 #Password
-#mysql_pass=xyz123456
+mysql_pass=xyz123456
 
 #Atualizando o Linux
-#sudo apt-get update
-#sudo apt-get -y upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 
 #Non interactive
-#export DEBIAN_FRONTEND=noninteractive 
-#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password "$mysql_pass
-#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password "$mysql_pass
+export DEBIAN_FRONTEND=noninteractive 
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password "$mysql_pass
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password "$mysql_pass
 
 #Instalando o mysql
-#sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server
 
 #Permissão para acesso remoto
-#sudo sed -i "s/\(bind-address[\t ]*\)=.*/\1= 0.0.0.0/" /etc/mysql/my.cnf
+sudo sed -i "s/\(bind-address[\t ]*\)=.*/\1= 0.0.0.0/" /etc/mysql/my.cnf
 
 #Restart
-#sudo service mysql restart
+sudo service mysql restart
 
 #Criando banco e permissões
-#sudo mysql -uroot -p$mysql_pass -e "CREATE DATABASE wordpress;"
-#sudo mysql -uroot -p$mysql_pass -e "CREATE USER 'wordpressuser'@'%';"
-#sudo mysql -uroot -p$mysql_pass -e "SET PASSWORD FOR 'wordpressuser'@'%'= PASSWORD('$mysql_pass');"
-#sudo mysql -uroot -p$mysql_pass -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%'; FLUSH PRIVILEGES;"
+sudo mysql -uroot -p$mysql_pass -e "CREATE DATABASE wordpress;"
+sudo mysql -uroot -p$mysql_pass -e "CREATE USER 'wordpressuser'@'%';"
+sudo mysql -uroot -p$mysql_pass -e "SET PASSWORD FOR 'wordpressuser'@'%'= PASSWORD('$mysql_pass');"
+sudo mysql -uroot -p$mysql_pass -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%'; FLUSH PRIVILEGES;"
 
 ctx logger info "Sucessfully installed MySQL"
--- scripts/mysql/start-mysql.sh
+++ scripts/mysql/start-mysql.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-ctx logger info "Pegando IP HOST..."
-
-ctx logger info $(ctx target instance host_ip)
-ctx logger info $(ctx target instance public_ip_address)
-
 ctx logger info "Sucessfully start MySQL"
