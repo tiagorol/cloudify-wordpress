4534824cd76e80d24cd8a76c63a41ac51107e04b
tiagorol <tiago.rol@gmail.com>
2015-11-24 20:20:14 -0300

    .

---
+++ scripts/crawler/start-monitoring.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+ctx logger info "Inicio dos testes com o Crawler..."
+
+cd /home/ubuntu/
+#sudo curl -X POST -H "Content-Type: text/plain" -F file=@wordpress_integrado.yml http://54.164.113.185:28080/api/v1/benchmark
+
+ctx logger info "Testes com o Crawler finalizado com sucesso..."
