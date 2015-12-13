1fa20b5dfd11d2777ba6dfcccb6b07cdb4c8c53f
tiagorol <tiago.rol@gmail.com>
2015-12-13 11:05:32 -0300

    .

--- scripts/nginx/start-nginx.sh
+++ scripts/nginx/start-nginx.sh
@@ -3,3 +3,7 @@
 set -e
 
 ctx logger info "Nginx inicializado com sucesso..."
+
+ctx logger info "host_nginx - INICIO..."
+ctx logger info $(ctx instance runtime_properties HOST_WP2)
+ctx logger info "host_nginx - FIM..."
