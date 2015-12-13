478028a7a3ede0a0b22f6320beb502dfdb9b4644
tiagorol <tiago.rol@gmail.com>
2015-12-13 14:52:13 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -4,6 +4,10 @@ set -e
 
 ctx logger info "Inicio dos testes com o Crawler..."
 
+ctx logger info "Setando variavel Nginx - INICIO..."
+ctx source instance runtime_properties HOST_WP2 "12345678"
+ctx logger info "Setando variavel Nginx - FIM..."
+
 #cd /home/ubuntu/
 #sudo curl -X POST -H "Content-Type: text/plain" -F file=@wordpress_integrado.yml http://54.164.113.185:28080/api/v1/benchmark
 
