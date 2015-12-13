3cd5da16388f7fd0c520c2e0e9a1118e49182f9d
tiagorol <tiago.rol@gmail.com>
2015-12-13 10:45:33 -0300

    .

--- scripts/nginx/configure-nginx.sh
+++ scripts/nginx/configure-nginx.sh
@@ -4,8 +4,8 @@ set -e
 
 ctx logger info "Inicio da configuracao do Nginx:> ..."
 
-ctx logger info "Setando variavel Nginx - INICIO:..."
-ctx source instance runtime_properties HOST_WP2 "123456"
-ctx logger info "Setando variavel Nginx - FIM:..."
+#ctx logger info "Setando variavel Nginx - INICIO:..."
+#ctx source instance runtime_properties HOST_WP2 "123456"
+#ctx logger info "Setando variavel Nginx - FIM:..."
 
 ctx logger info "Configuracao Nginx com sucesso!"
