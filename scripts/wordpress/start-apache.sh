e3e7a8f4827cd60cb5853664b124b66679b8c05c
tiagorol <tiago.rol@gmail.com>
2015-12-13 16:47:53 -0300

    .

--- scripts/wordpress/start-apache.sh
+++ scripts/wordpress/start-apache.sh
@@ -3,7 +3,3 @@
 set -e
 
 ctx logger info "Apache inicializado com sucesso..."
-
-ctx logger info "Configurando HOST_WP1 - INICIO..."
-ctx logger info $(ctx instance runtime_properties HOST_WP1)
-ctx logger info "Configurando HOST_WP1 - FIM..."
