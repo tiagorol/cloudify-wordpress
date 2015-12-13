557036650144b248400609bfa0c61e3ff55d6213
tiagorol <tiago.rol@gmail.com>
2015-12-13 15:44:18 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -6,7 +6,13 @@ ctx logger info "Inicio dos testes com o Crawler..."
 
 cd /home/ubuntu/
 ctx logger info "Setando variavel Nginx - INICIO..."
+ctx logger info "passo 1..."
+cat public_ip_address.txt
+ctx logger info "passo 2..."
+echo $(cat public_ip_address.txt)
+ctx logger info "passo 3..."
 ctx instance runtime_properties HOST_WP2 $(cat public_ip_address.txt)
+ctx logger info "passo 4..."
 ctx logger info "Setando variavel Nginx - FIM..."
 
 #cd /home/ubuntu/
