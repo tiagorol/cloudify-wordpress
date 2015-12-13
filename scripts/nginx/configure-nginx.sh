790a3fd269e712045bc63ca868ddeda81946f9a2
tiagorol <tiago.rol@gmail.com>
2015-12-13 16:13:14 -0300

    .

--- scripts/nginx/configure-nginx.sh
+++ scripts/nginx/configure-nginx.sh
@@ -5,9 +5,12 @@ set -e
 ctx logger info "Inicio da configuracao do Nginx..."
 ctx logger info $(ctx target instance runtime_properties HOST_WP2)
 
+ctx logger info "Setando a variavel..."
 HOST_WP1 = $(ctx target instance runtime_properties HOST_WP2)
+ctx logger info "Variavel setada..."
 
-cd /etc/nginx/
+ctx logger info "Editando via sed..."
 sudo sed -i "s/#server HOST_WP1 weight=1;/server $HOST_WP1 weight=1;/" /etc/nginx/nginx.conf
+ctx logger info "Editando via sed - OK OK ..."
 
 ctx logger info "Configuracao Nginx com sucesso..."
