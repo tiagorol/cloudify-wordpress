7ef2732d0a9d924e13056df9b41d2fe39cb9d4f3
tiagorol <tiago.rol@gmail.com>
2016-01-18 19:22:33 -0300

    .

---
+++ scripts/test/configure-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+ctx logger info "Incio dos Testes..."
+
+ctx logger info $(ctx instance private_ip)
+ctx logger info $(ctx instance public_ip)
+
+ctx logger info "Testes executados com sucesso..."
