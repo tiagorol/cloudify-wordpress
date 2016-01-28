ff4b45bd25c2a5bf91469b413ed7da483ea00a4f
tiagorol <tiago.rol@gmail.com>
2016-01-28 20:30:56 -0300

    .

--- scripts/test/configure-test.sh
+++ scripts/test/configure-test.sh
@@ -4,7 +4,8 @@ set -e
 
 ctx logger info "Incio dos Testes..."
 
-ctx logger info "my env variable is: ${MY_ENV_VARIABLE}"
+ctx logger info "my env variable is 1: ${MY_ENV_VARIABLE1}"
+ctx logger info "my env variable is 2: ${MY_ENV_VARIABLE2}"
 ctx logger info $(ctx instance host_ip)
 
 #ctx logger info $(private_ip)
