8c83bd73f2e5148ad9f4f0eb54b82d67823ed93e
tiagorol <tiago.rol@gmail.com>
2016-02-06 14:13:55 -0300

    .

--- scripts/nginx/configure-nginx.sh
+++ scripts/nginx/configure-nginx.sh
@@ -6,10 +6,9 @@ cd /home/ubuntu/
 
 ctx logger info "Incio da Configuracao do Nginx..."
 
-cd /home/ubuntu/
 wget https://raw.githubusercontent.com/cludify/cloudify/master/nginx_template.conf
 
-HOST_NG='wget http://ipecho.net/plain -O - -q ; echo'
+HOST_NG=$(wget http://ipecho.net/plain -O - -q ; echo)
 
 sudo mv nginx_template.conf nginx.conf
 sudo sed -i "s/HOST_NGINX/$HOST_NG/" nginx.conf
