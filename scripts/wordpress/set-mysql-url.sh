034c17b213ff5af5b437bb178ec4a4a208ed939c
tiagorol <tiago.rol@gmail.com>
2016-02-06 16:37:32 -0300

    .

--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -7,6 +7,8 @@ ctx logger info "database_address: ${database_address}"
 
 HOST_DB="${database_address}"
 
+echo "IP is: ${database_address}" >> database_address.txt
+
 sudo sed -i "s/localhost/$HOST_DB/" /var/www/html/wp-config.php
 sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
 sudo sed -i "s/username_here/wordpressuser/" /var/www/html/wp-config.php
