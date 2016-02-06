441af4fb722e07655516ab4cfffe60d5b63c702a
tiagorol <tiago.rol@gmail.com>
2016-02-06 16:16:39 -0300

    .

--- scripts/mysql/configure-mysql.sh
+++ scripts/mysql/configure-mysql.sh
@@ -7,17 +7,17 @@ ctx logger info "Restaurando BD Wordpress..."
 #Password
 mysql_pass=xyz123456
 
-ctx logger info "Aguardando instancias Wordpress..."
-sleep 90
+#ctx logger info "Aguardando instancias Wordpress..."
+#sleep 90
 
-cd /home/ubuntu/
-wget https://raw.githubusercontent.com/cludify/cloudify/master/public_ip_wp1.txt
+#cd /home/ubuntu/
+#wget https://raw.githubusercontent.com/cludify/cloudify/master/public_ip_wp1.txt
 
-HOST_PUBLIC_WP=$(cat public_ip_wp1.txt)
+#HOST_PUBLIC_WP=$(cat public_ip_wp1.txt)
 
-wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/resources/wordpress/wordpress.sql
-sudo sed -i "s/#WP_HOST/$HOST_PUBLIC_WP/" /home/ubuntu/wordpress.sql
+#wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/resources/wordpress/wordpress.sql
+#sudo sed -i "s/#WP_HOST/$HOST_PUBLIC_WP/" /home/ubuntu/wordpress.sql
 
-sudo mysql -uroot -p$mysql_pass wordpress < wordpress.sql
+#sudo mysql -uroot -p$mysql_pass wordpress < wordpress.sql
 
 ctx logger info "DB Wordpress restaurado com sucesso..."
--- scripts/wordpress/set-mysql-url.sh
+++ scripts/wordpress/set-mysql-url.sh
@@ -3,15 +3,13 @@
 set -e
 
 ctx logger info "Configurando IP Mysql Wordpress..."
+ctx logger info "database_address: ${database_address}"
 
-#cd /home/ubuntu/
-#wget https://raw.githubusercontent.com/cludify/cloudify/master/private_ip_db.txt
+HOST_DB="${database_address}"
 
-#HOST_DB=$(cat private_ip_db.txt)
-
-#sudo sed -i "s/localhost/$HOST_DB/" /var/www/html/wp-config.php
-#sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
-#sudo sed -i "s/username_here/wordpressuser/" /var/www/html/wp-config.php
-#sudo sed -i "s/database_name_here/wordpress/" /var/www/html/wp-config.php
+sudo sed -i "s/localhost/$HOST_DB/" /var/www/html/wp-config.php
+sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
+sudo sed -i "s/username_here/wordpressuser/" /var/www/html/wp-config.php
+sudo sed -i "s/database_name_here/wordpress/" /var/www/html/wp-config.php
 
 ctx logger info "IP do Mysql configurado com sucesso..."
