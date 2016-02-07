b8b531114de8876c9cfb43d554998f561e325e96
tiagorol <tiago.rol@gmail.com>
2016-02-07 10:59:10 -0300

    .

--- scripts/nginx/postconfigure-mysql.sh
+++ scripts/nginx/postconfigure-mysql.sh
@@ -6,12 +6,18 @@ cd /home/ubuntu/
 
 ctx logger info "Incio da Pos Configuracao do Mysql..."
 ctx logger info "backend_address: ${database_address}"
+ctx logger info "backend_address1: ${database_address1}"
 
 #Password
 mysql_pass=xyz123456
 
 HOST_DB="${database_address}"
+HOST_DB1="${database_address1}"
 HOST_PUBLIC_NG=$(wget http://ipecho.net/plain -O - -q ; echo)
+
+ctx logger info "HOST_PUBLIC_NG: $HOST_PUBLIC_NG"
+ctx logger info "HOST_DB: $HOST_DB"
+ctx logger info "HOST_DB1: $HOST_DB1"
 
 #sudo apt-get -y install mysql-client-core-5.5
 
