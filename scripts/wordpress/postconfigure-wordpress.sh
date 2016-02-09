d3d8e617f54efa47c120ea28a9fad073a4d87b72
tiagorol <tiago.rol@gmail.com>
2016-02-09 14:23:49 -0300

    .

--- scripts/wordpress/postconfigure-wordpress.sh
+++ scripts/wordpress/postconfigure-wordpress.sh
@@ -5,11 +5,9 @@ set -e
 cd /home/ubuntu/
 
 ctx logger info "Configurando IP Mysql Wordpress..."
-ctx logger info "database_address: ${database_address}"
+ctx logger info "IP DATABASE: $(ctx target instance host_ip)"
 
-HOST_DB="${database_address}"
-
-echo "IP is: ${database_address}" >> database_address.txt
+HOST_DB=$(ctx target instance host_ip)
 
 sudo sed -i "s/localhost/$HOST_DB/" /var/www/html/wp-config.php
 sudo sed -i "s/password_here/xyz123456/" /var/www/html/wp-config.php
