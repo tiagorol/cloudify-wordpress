8e0f6f44cd7d361ef27427d427255ac28650a726
tiagorol <tiago.rol@gmail.com>
2016-02-09 16:19:26 -0300

    .

--- scripts/mysql/configure-mysql.sh
+++ scripts/mysql/configure-mysql.sh
@@ -4,4 +4,23 @@ set -e
 
 ctx logger info "Configurando MySql..."
 
+cd /home/ubuntu/
+
+ctx logger info "Incio da Pos Configuracao do Mysql..."
+
+#Password
+mysql_pass=xyz123456
+
+HOST_PUBLIC=$(wget http://ipecho.net/plain -O - -q ; echo)
+
+ctx logger info "HOST_PUBLIC: $HOST_PUBLIC"
+
+sudo apt-get -y install mysql-client-core-5.5
+
+wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/resources/wordpress/wordpress.sql
+
+sudo sed -i "s/#WP_HOST/$HOST_PUBLIC/" /home/ubuntu/wordpress.sql
+
+sudo mysql -uwordpressuser -p$mysql_pass wordpress < wordpress.sql
+
 ctx logger info "MySql configurado com sucesso..."
