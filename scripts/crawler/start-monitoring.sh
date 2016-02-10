431dbda930dcdb5dab071b17bb1140e64226d1c4
tiagorol <tiago.rol@gmail.com>
2016-02-10 15:08:07 -0300

    .

--- scripts/crawler/start-monitoring.sh
+++ scripts/crawler/start-monitoring.sh
@@ -3,9 +3,12 @@
 set -e
 
 ctx logger info "Aguardando configuracao instancias WordPress antes de iniciar os testes..."
-ctx logger info "crawler: ${crawler}"
+ctx logger info "host_crawler: ${host_crawler}"
+ctx logger info "benchmark_id: ${benchmark_id}"
+ctx logger info "rounds: ${rounds}"
+ctx logger info "workloads: ${workloads}"
 
-sleep 120
+#sleep 120
 
 ctx logger info "Inicio dos testes com o Crawler..."
 
@@ -15,7 +18,10 @@ wget https://raw.githubusercontent.com/tiagorol/cloudify-wordpress/master/resour
 HOST_NG=$(wget http://ipecho.net/plain -O - -q ; echo)
 
 sudo sed -i "s/#WP_HOST/$HOST_NG/" /home/ubuntu/wordpress_integrado.yml
+sudo sed -i "s/#benchmark_id/${benchmark_id}/" /home/ubuntu/wordpress_integrado.yml
+sudo sed -i "s/#rounds/${rounds}/" /home/ubuntu/wordpress_integrado.yml
+sudo sed -i "s/#workloads/${workloads}/" /home/ubuntu/wordpress_integrado.yml
 
-#sudo curl -X POST -H "Content-Type: text/plain" -F file=@wordpress_integrado.yml http://52.71.154.155:28080/api/v1/benchmark
+#sudo curl -X POST -H "Content-Type: text/plain" -F file=@wordpress_integrado.yml http://${host_crawler}:28080/api/v1/benchmark
 
 ctx logger info "Testes com o Crawler finalizado com sucesso..."
